4f22357297d43b5c87e071a059cb9c5f36da0125
sjoonb <sungjun9808@gmail.com>
2023-03-29 21:16:04 +0900

    Tailwind 적용

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,18 +1,11 @@
 import { useState } from 'react';
-import './Popup.css';
 
 function App() {
   const [crx, setCrx] = useState('create-chrome-ext');
 
   return (
     <main>
-      <h3>Popup Page!</h3>
-
-      <h6>v 0.0.0</h6>
-
-      <a href="https://www.npmjs.com/package/create-chrome-ext" target="_blank">
-        Power by {crx}
-      </a>
+      <h1 className="text-blue-600 font-bold">Hello world!</h1>
     </main>
   );
 }
