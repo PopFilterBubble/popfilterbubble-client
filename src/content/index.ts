d9ac1e905c8646ee66175c05ef30e721941fda3a
jimin <moana16@naver.com>
2023-04-10 19:22:38 +0900

    feat/data : 채널Id 가져옴 #4

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,27 +1,18 @@
-console.info('chrome-ext template-react-ts content script');
-
+console.info('pop-filterbubble-client content script');
 const root = document.documentElement;
-let videoList = root.querySelectorAll('#video-title-link');
-let LinktoArr = Array.from(videoList);
-LinktoArr.map((a,idx) => {
-  if (a instanceof HTMLAnchorElement) {
-    let href = a.href; 
-    let params = href .substring( href .indexOf('=')+1, href .length );
-    console.log(idx, params);
-  }
-});
 
-//개수가 24개 이하면 처리
-
-/*function getVideoId () {
-    for(let i :number = 0; i < videoList.length; i++) {
-        if (videoList[i] instanceof HTMLAnchorElement) {
-            let href = videoList[i].href; 
-          }
-        
+setInterval(()=> {
+  const videoList = root.querySelectorAll('#text > a');
+  const linkToArr = Array.from(videoList);
+  
+  const newVideoList = linkToArr.map((a,idx) => {
+    if (a instanceof HTMLAnchorElement) {
+      let href = a.href; 
+      let channelId = href .replace('https://www.youtube.com/','');
+      return channelId;
     }
-
-};
-console.log(root.innerHTML);*/
+  });
+  console.log(newVideoList);
+},5000);
 
 export {};
